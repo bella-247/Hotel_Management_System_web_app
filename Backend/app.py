1864de690304fe311a9b3ab7dcaded4b1426eb36
abelmekonen <abelmekonen247@gmail.com>
2025-01-27 04:34:17 +0300

    Ignored all the .png files, cause it takes time to upload them

--- Backend/app.py
+++ Backend/app.py
@@ -5,20 +5,4 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysql-connector://root:MHMABN1&6abel@localhost/'
-
-users -> id (INT, primary key, auto_increment)
-    -> username string (20)
-    -> password string(50)
-    -> created_at (datatime)
-
-staffs -> id(same shit)
-    -> name (same)
-    -> email string(50)
-    -> password (same)
-    -> created_at (same)
-
-rooms -> id(same)
-
-
-"General Manager", "Assitant General Manager", "Operations Manager", "Finance Manager", "Human Resource Manager", 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysql-connector://root:@localhost/'
